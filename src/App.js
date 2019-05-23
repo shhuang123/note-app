ca85f59597b36d3daea4a83ab7fac9a6846218be
shhuang123 <sharonhuang923@gmail.com>
2019-05-23 19:19:33 +0000

    delete yarn file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -16,6 +17,10 @@ class App extends Component {
     this.setState({
       showNote: ! this.state.showNote
     });
+  }
+
+  getNotes = () => {
+    axios.get('https://firehose-note-api.herokuapp.com/notes')
   }
 
   render() {
