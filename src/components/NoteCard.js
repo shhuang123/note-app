0b6ca011e0fee1254e09074ef20f202da90a9556
shhuang123 <sharonhuang923@gmail.com>
2019-05-25 20:02:18 +0000

    delete button can properly remove items

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class NoteCard extends React.Component {
   render() {
-    const { note, getNote } = this.props;
+    const { note, getNote, deleteNote } = this.props;
 
     return (
       <div className="note-card-container">
@@ -15,7 +15,7 @@ class NoteCard extends React.Component {
         <span className="note-card-delete">
           <i className="material-icons">close</i>
         </span>
-        <span className="note-card-edit" onClick={() => getNote(note.id)}>
+        <span className="note-card-delete" onClick={() => deleteNote(note.id)}>
           <i className="material-icons">mode_edit</i>
         </span>
       </div>
